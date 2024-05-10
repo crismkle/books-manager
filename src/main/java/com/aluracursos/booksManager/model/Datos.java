bf739d3cc2024dd41254c193b1583a7c61af3e63
CristianM <crism.klee@gmail.com>
2024-05-10 20:20:00 -0300

    Top 10 y registros

--- src/main/java/com/aluracursos/booksManager/model/Datos.java
+++ src/main/java/com/aluracursos/booksManager/model/Datos.java
@@ -7,10 +7,7 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
-public record DatosLibro(
-        @JsonAlias("title") String titulo,
-        @JsonAlias("authors") List<DatosAutor> autor,
-        @JsonAlias("languages") List<String> idiomas,
-        @JsonAlias("download_count") Double descargas
+public record Datos(
+        @JsonAlias("results") List<DatosLibro> resultados
 ) {
 }
--- src/main/java/com/aluracursos/booksManager/principal/Principal.java
+++ src/main/java/com/aluracursos/booksManager/principal/Principal.java
@@ -1,23 +1,36 @@
 package com.aluracursos.booksManager.principal;
 
+import com.aluracursos.booksManager.model.Datos;
 import com.aluracursos.booksManager.model.DatosLibro;
 import com.aluracursos.booksManager.service.ConsumoAPI;
 import com.aluracursos.booksManager.service.ConvierteDatos;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner teclado = new Scanner(System.in);
-    private final String URL_BASE = "https://gutendex.com/books/?search=world";
+    private final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
 
     public void mostrarMenu(){
 
         var json = consumoAPI.obtenerDatos(URL_BASE);
-        var datos = conversor.obtenerDatos(json, DatosLibro.class);
         System.out.println(json);
+        var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        // TOP 10 Libros más descargados
+        System.out.println("Top 10 de libros más descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibro::descargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
+        
+
     }
 }
