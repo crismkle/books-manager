f925f100c8408265e2a1afeec26510ba12c4e723
CristianM <crism.klee@gmail.com>
2024-05-10 21:14:41 -0300

    Busqueda y estadisticas

--- src/main/java/com/aluracursos/booksManager/principal/Principal.java
+++ src/main/java/com/aluracursos/booksManager/principal/Principal.java
@@ -6,7 +6,10 @@ import com.aluracursos.booksManager.service.ConsumoAPI;
 import com.aluracursos.booksManager.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -23,14 +26,39 @@ public class Principal {
         System.out.println(datos);
 
         // TOP 10 Libros más descargados
-        System.out.println("Top 10 de libros más descargados");
+        System.out.println("\nTop 10 de libros más descargados");
         datos.resultados().stream()
                 .sorted(Comparator.comparing(DatosLibro::descargas).reversed())
                 .limit(10)
                 .map(l -> l.titulo().toUpperCase())
                 .forEach(System.out::println);
 
-        
+        // Búsqueda de libros por nombre
+        System.out.println("\nIngrese el nombre del libro que desea buscar");
+        var tituloLibro = teclado.nextLine();
+        json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + tituloLibro.replace(" ", "+"));
+        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+
+        Optional<DatosLibro> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+
+        if(libroBuscado.isPresent()){
+            System.out.println("Libro encontrado ");
+            System.out.println(libroBuscado.get());
+        }else{
+            System.out.println("Libro no encontrado");
+        }
+
+        // Trabajando con estadísticas
+        DoubleSummaryStatistics est = datos.resultados().stream()
+                .filter(d -> d.descargas() > 0)
+                .collect(Collectors.summarizingDouble(DatosLibro::descargas));
+
+        System.out.println("\nCantidad media de descargas: " + est.getAverage());
+        System.out.println("Cantidad máxima de descargas: " + est.getMax());
+        System.out.println("Cantidad mínima de descargas: " + est.getMin());
+        System.out.println("Cantidad de registros evaluados para calcular las estadísticas: " + est.getCount());
 
     }
 }
